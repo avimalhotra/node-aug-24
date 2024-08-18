aa6bc955f5ff2fe867866dbf34444cb3c6026dfc
Avinash <avimalhotra505@gmail.com>
2024-08-18 11:04:01 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+//require('colors');                  // from npm
+//const os=require('os');             // build in
+//const x=require('./ext');           // from app
+import {area} from './ext.js';        // ES Module
+
+
+// console.log(`Hello Node JS, version: ${process.version}`);
+// console.log("CYAN".cyan);
+// console.log("MAGENTA".magenta);
+// console.log("RED".red);
+// console.log("YELLOW".yellow);
+
+
+// console.log(x);
+//console.log(area);
+
+//console.log(os.cpus().length);
+//console.log(os.freemem()/1024/1024/1024 +" / "+ os.totalmem()/1024/1024/1024);
