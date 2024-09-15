375ba9adb59d1d7d19c15c6803a4c8bf2ae0bb49
Avinash <avimalhotra505@gmail.com>
2024-09-15 11:02:59 +0530

    session and multer

--- src/app.js
+++ src/app.js
@@ -5,10 +5,22 @@ const path=require("path");
 const app=express();
 const admin=require("./routes/admin");
 const cars=require("./routes/cars");
+//const parseurl=require('parseurl');
 
 
-const cookieParser=require('cookie-parser');
-app.use(cookieParser("secret"));
+// const session=require("express-session");
+// // trust first proxy
+// app.set('trust proxy', 1); 
+// app.use(session({
+//     secret:"session",
+//     resave:false,
+//     saveUninitialized:true,
+//     cookie:{secure:false,maxAge:(1000 * 3)}
+// }));
+
+
+// const cookieParser=require('cookie-parser');
+// app.use(cookieParser("secret"));
 
 
 const bodyParser=require('body-parser');
@@ -16,17 +28,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 
 
+const multer  = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'src/public/uploads/');
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);          // for original name 
+      //cb(null, Date.now() + path.extname(file.originalname)) 
+    }
+  });
+
+// const upload = multer({ storage:storage });
+
+
 /* router */
 app.use("/admin",admin);
 app.use("/cars",cars);
 
- //app.use(express.static(path.resolve("src/public")));
- //app.use(express.static(path.resolve("node_modules/bootstrap/dist")));
+ app.use(express.static(path.resolve("src/public")));
+ app.use(express.static(path.resolve("node_modules/bootstrap/dist")));
 
 // app.use((req,res,next)=>{
-//     console.log("Use Login");
+//     if(!req.session.views){
+//         req.session.views={};
+//     }
+
+//     let pathname = parseurl(req).pathname;
+
+//     req.session.views.pathname=(req.session.views.pathname || 0) +1;
+    
 //     next();
 // });
+
+
 
 
 app.get("/",(req,res)=>{
@@ -34,12 +69,13 @@ app.get("/",(req,res)=>{
 
     // res.cookie("fb-token","1234567893456789",{signed:true});
     // res.status(200).send(req.cookies);
-    res.cookie("city","noida", {maxAge:86400000, httpOnly: true})
+    //res.cookie("city","noida", {maxAge:86400000, httpOnly: true})
 
-    res.status(200).send(req.signedCookies);
+    //res.status(200).send(req.signedCookies);
     // res.status(200).send(Buffer.from("abcd"));
-    //res.status(200).send("Hello World");
-    // res.status(200).send("<h1>Hello World</h1>");
+    // res.status(200).send("Hello World");
+    //res.status(200).send(` Session Id : ${req.sessionID}, page views: ${req.session.views.pathname}`);
+    res.status(200).send("<h1>Hello World</h1>");
 });
 
 app.get("/api",(req,res)=>{
@@ -59,6 +95,25 @@ app.post('/login',(req,res)=>{
     console.log(req.body);
     res.status(200).send(`Email: ${req.body.email}, password: ${req.body.password}`);
 });
+
+// app.post('/upload', upload.single('pic'),(req,res)=>{
+//     console.log(req.file);
+//     res.status(200).send("file uploaded");
+// });
+const upload = multer().single('pic');
+app.post('/upload',(req,res)=>{
+        upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                console.error( err );
+            } else if (err) {
+                console.error( err );
+            }
+            else{
+                res.status(200).send("file uploaded");
+            }
+        })
+});
+
 
 /* wild card handler */
 app.get("/**",(req,res)=>{
